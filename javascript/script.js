84997d996eac345ca91d2048dcb2ffe3db2c5903
Max W <mwinter@wesleyan.edu>
2022-08-21 15:46:41 -0500

    multiple filter values

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,18 @@
-const filterSelection = (choice) => {
-    var cards, index;
+const filterSelection = (choices) => {
+    console.log(choices);
+    var cards, card_index, choice_index;
     cards = document.getElementsByClassName("card");
-    if (choice === "all") choice = "";
-    for (index = 0; index < cards.length; index++) {
-        removeClass(cards[index],"show");
-        if (cards[index].className.indexOf(choice) > -1) {
-            addClass(cards[index], "show");
-        }
+    for (choice_index = 0; choice_index < choices.length; choice_index++) {
+        if (choices[choice_index] === "all") choices[choice_index] = "";
     }
-
+    for (card_index = 0; card_index < cards.length; card_index++) {
+        removeClass(cards[card_index],"show");
+        if (cards[card_index].className.indexOf(choices[0]) > -1 
+            && 
+            cards[card_index].className.indexOf(choices[1]) > -1) {
+                addClass(cards[card_index], "show");
+            }
+    }
 };
 
 const removeClass = (card, className) => {
@@ -34,4 +38,18 @@ const addClass = (card, className) => {
     }
 };
 
-//filterSelection("all");
+//var partner_filter = document.getElementById("type_filter");
+//var region_filter = document.getElementById("region_filter");
+var filter_form = document.getElementById("filters");
+//partner_filter.addEventListener("change", filterSelection([partner_filter.value, region_filter.value]));
+//region_filter.addEventListener("change", filterSelection([partner_filter.value,region_filter.value]));
+filter_form.addEventListener("submit", filterSelection(["technology","all"]));
+
+// choices
+// variation 1: ["all", "all"]
+// variation 2: ["reseller", "all"]
+// variation 3: ["reseller", "usa"]
+// variation 4: ["all", "usa"]
+
+// cards
+// variations 1: ["card technology usa show"]
